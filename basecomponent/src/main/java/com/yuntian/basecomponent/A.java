4a088499b50532e9a508ea6230230d3a9a8142b0
yuntian <944610721@qq.com>
2018-12-18 21:11:12 +0800

    TabLayout的androidx适配

---
+++ basecomponent/src/main/java/com/yuntian/basecomponent/A.java
@@ -0,0 +1,9 @@
+package com.yuntian.basecomponent;
+
+/**
+ * @author chulingyan
+ * @time 2018/12/18 21:04
+ * @describe
+ */
+public class A {
+}
---
+++ basecomponent/src/main/java/com/yuntian/basecomponent/tablayout/listener/OnTabSelectListener.java
@@ -0,0 +1,6 @@
+package com.yuntian.basecomponent.tablayout.listener;
+
+public interface OnTabSelectListener {
+    void onTabSelect(int position);
+    void onTabReselect(int position);
+}
